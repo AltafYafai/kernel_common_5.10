419e004d3a548bb6be5af5c94e93bf8bceea6656
XTENSEI <xtensei.is.not.in.the.sudoers.file@protonmail.com>
2026-05-13 11:39:11 +0000

    hikari: default to enabled at boot
    Change hikari_enable_value initial value from 0 to 1 so Hikari is
    active immediately after init completes.  Combined with the top-app
    auto-opt-in, foreground tasks get boosted without any user action.
    
    Kill switch remains functional:
      echo 0 > /proc/sys/kernel/hikari_enable
    
    Reverts to enabled on next reboot.
    
    Signed-off-by: XTENSEI <xtensei.is.not.in.the.sudoers.file@protonmail.com>

--- kernel/sched/hikari.c
+++ kernel/sched/hikari.c
@@ -99,7 +99,7 @@ static DEFINE_PER_CPU(struct hikari_pcpu, hikari_pcpu);
  * pass through proc_douintvec_minmax which is a WRITE_ONCE-like
  * store on the slow path.
  */
-static unsigned int hikari_enable_value;
+static unsigned int hikari_enable_value = 1;
 static unsigned int hikari_wake_threshold_us = 1000;
 static unsigned int hikari_uclamp_boost_pct = 30;
 static unsigned int hikari_uclamp_ttl_ms = 16;
