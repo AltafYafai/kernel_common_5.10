e05c574721d68645420fcf4f826fae1ff487b979
アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>
2026-06-20 10:49:50 +0800

    herald: fix comment and __ATTR macro build errors
    The shell example in the header comment contained /*/ which
    terminated the block comment prematurely.
    
    __ATTR_RO/WO macros expect functions named after the attribute
    (name_show, value_show, etc.) but the actual functions were
    named herald_entry_{name,val}_show, etc. Use explicit __ATTR()
    definitions with the correct function pointers.

--- drivers/misc/vindicator/herald.c
+++ drivers/misc/vindicator/herald.c
@@ -15,16 +15,8 @@
  *   - Each entry is pairs of files (name + value) for easy parsing.
  *   - Supports deletion (herald_del_prop) for cancelling pending props.
  *
- * Userspace usage (in init.rc or daemon):
- *   while true; do
- *     for d in /sys/kernel/herald/queue/*/; do
- *       name=$(cat $d/name)
- *       val=$(cat $d/value)
- *       setprop $name $val
- *       echo 1 > $d/commit
- *     done
- *     sleep 2
- *   done
+ * Userspace: poll /sys/kernel/herald/queue/<name>/{name,value,commit}
+ * and call setprop(name, value). Each entry's commit file consumes it.
  *
  * Author: GrayRavens
  */
@@ -75,7 +67,7 @@ static ssize_t herald_entry_name_show(struct kobject *kobj,
 	struct herald_entry *e = container_of(kobj, struct herald_entry, kobj);
 	return sysfs_emit(buf, "%s\n", e->name);
 }
-static struct kobj_attribute herald_attr_name = __ATTR_RO(name);
+static struct kobj_attribute herald_attr_name = __ATTR(name, 0444, herald_entry_name_show, NULL);
 
 static ssize_t herald_entry_val_show(struct kobject *kobj,
 				     struct kobj_attribute *attr, char *buf)
@@ -83,7 +75,7 @@ static ssize_t herald_entry_val_show(struct kobject *kobj,
 	struct herald_entry *e = container_of(kobj, struct herald_entry, kobj);
 	return sysfs_emit(buf, "%s\n", e->val);
 }
-static struct kobj_attribute herald_attr_val = __ATTR_RO(value);
+static struct kobj_attribute herald_attr_val = __ATTR(value, 0444, herald_entry_val_show, NULL);
 
 static ssize_t herald_entry_commit_store(struct kobject *kobj,
 					 struct kobj_attribute *attr,
@@ -104,7 +96,7 @@ static ssize_t herald_entry_commit_store(struct kobject *kobj,
 	}
 	return count;
 }
-static struct kobj_attribute herald_attr_commit = __ATTR_WO(commit);
+static struct kobj_attribute herald_attr_commit = __ATTR(commit, 0200, NULL, herald_entry_commit_store);
 
 static struct attribute *herald_entry_attrs[] = {
 	&herald_attr_name.attr,
@@ -134,7 +126,7 @@ static ssize_t herald_depth_show(struct kobject *kobj,
 {
 	return sysfs_emit(buf, "%d\n", atomic_read(&herald_queue_depth));
 }
-static struct kobj_attribute herald_attr_depth = __ATTR_RO(queue_depth);
+static struct kobj_attribute herald_attr_depth = __ATTR(queue_depth, 0444, herald_depth_show, NULL);
 
 static struct attribute *herald_attrs[] = {
 	&herald_attr_depth.attr,
